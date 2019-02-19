6d793f9907977294bee6acfb5a509d17ab09b0b0
Michael Steidl <mwsteidl@newsit.biz>
2019-02-19 22:21:30 +0100

    First unit test implemented

---
+++ tests/plainval01.test.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_1 = require("../src/index");
+var assert = require("assert");
+function InitOfo() {
+    var testofo = new index_1.ObjectFromObject();
+    testofo.loadSourceObject("", "./tests/sourceRef02.json");
+    testofo.loadRules("", "./tests/buildruleRefA2.json");
+    testofo.buildTargetFromSourceByRules();
+    var builtobject = testofo.getTargetObject();
+    return builtobject;
+}
+/* TEST */
+describe("Plain Value Test 01", function () {
+    it('should return *Value of bObj.bObj3.bObj3a*', function () {
+        var builtobject = InitOfo();
+        var testprop1 = builtobject["tPlain3"];
+        assert.deepStrictEqual(testprop1, "Value of bObj.bObj3.bObj3a");
+        // expect(testprop1).to.equal("Value of bObj.bObj3.bObj3a");
+    });
+});
---
+++ tests/plainval01.test.ts
@@ -0,0 +1,24 @@
+import {ObjectFromObject as Ofo} from "../src/index";
+import chai = require('chai');
+import assert = require('assert');
+import { expect } from "chai";
+
+function InitOfo(): object {
+    const testofo = new Ofo();
+    testofo.loadSourceObject("", "./tests/sourceRef02.json");
+    testofo.loadRules("", "./tests/buildruleRefA2.json");
+    testofo.buildTargetFromSourceByRules();
+    const builtobject: object = testofo.getTargetObject();
+    return builtobject;
+}
+
+/* TEST */
+describe("Plain Value Test 01", () => {
+    it('should return *Value of bObj.bObj3.bObj3a*',
+        () => {
+            let builtobject: any = InitOfo();
+            const testprop1: string = builtobject["tPlain3"];
+            assert.deepStrictEqual(testprop1, "Value of bObj.bObj3.bObj3a");
+            // expect(testprop1).to.equal("Value of bObj.bObj3.bObj3a");
+        })
+});
